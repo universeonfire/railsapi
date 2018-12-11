81313e2d10f44c7f38b121f56b3f34254a7f4cdf
Evren <evren@Evren-iMac.local>
2018-12-11 12:22:16 +0300

    tüm sistemler hazır

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |d|
+	FreelanceDocument.create!(title:"Document #{d}", 
+		description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, 
+		similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+		file_url: "https://docs.google.com/document/d/1QBc1zleJZqLdOT47Bn0YXsbFItkd2R6TpPDpQwl74lQ/edit?usp=sharing",
+		image_url:"http://aktiffelsefebursa.org/wp-content/uploads/cicerostatue-660x330.jpg",
+		)
+end
